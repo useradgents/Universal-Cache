e35e0878c73c1d016a8c5375cd18fc272d9b8bb1
Sylvain Puccinelli <s.puccinelli@useradgents.com>
2024-03-22 14:07:58 +0100

    update demo app

--- app/src/main/java/crocodile8/universal_cache_app/MainActivity.kt
+++ app/src/main/java/crocodile8/universal_cache_app/MainActivity.kt
@@ -4,11 +4,13 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
+import crocodile8.universal_cache.CachedSource
 import crocodile8.universal_cache.CachedSourceNoParams
 import crocodile8.universal_cache.FromCache
-import crocodile8.universal_cache.get
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -19,53 +21,55 @@ import java.util.concurrent.atomic.AtomicInteger
 class MainActivity : AppCompatActivity() {
 
     private val taskInvocationCnt = AtomicInteger()
-    private val longRunningTask = {
-        Thread.sleep(1000)
-        Log.i("test_", "test_ longRunningTask")
-        taskInvocationCnt.incrementAndGet()
-    }
-    private val source = CachedSourceNoParams(longRunningTask)
+    private val longRunningTask =
+        {
+            Thread.sleep(1000)
+            Log.i("test_", "test_ longRunningTask")
+            taskInvocationCnt.incrementAndGet()
+        }
+
+    private val source1 = CachedSource<String, Int>(source = { params -> longRunningTask() })
 
     private lateinit var textView1: TextView
     private lateinit var textView2: TextView
+    private lateinit var button1: Button
+    private lateinit var button2: Button
+    private lateinit var clear: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textView1 = findViewById(R.id.textView_1)
+
         textView2 = findViewById(R.id.textView_2)
-
-        launchAndObserveParallelRequests()
-        observeUpdates()
-    }
-
-    private fun launchAndObserveParallelRequests() {
-        lifecycleScope.launch {
-            source.get(FromCache.IF_HAVE)
-                .collect {
-                    Log.i("test_", "test_ collect 1: $it")
-                    textView1.text = "Ready, invocation count: $it"
-                }
-            source.get(FromCache.IF_HAVE)
-                .collect {
-                    Log.i("test_", "test_ collect 2: $it")
-                    textView1.text = "Ready, invocation count: $it"
-                }
-            source.get(FromCache.IF_HAVE)
-                .collect {
-                    Log.i("test_", "test_ collect 3: $it")
-                    textView1.text = "Ready, invocation count: $it"
-                }
+        button1 = findViewById(R.id.download_1)
+        button1.setOnClickListener {
+            refresh1()
+        }
+        button2 = findViewById(R.id.download_2)
+        button2.setOnClickListener {
+            refresh2()
+        }
+        clear = findViewById(R.id.clearCache)
+        clear.setOnClickListener {
+            lifecycleScope.launch { source1.clearCache() }
         }
     }
 
-    private fun observeUpdates() {
+    private fun refresh1() {
         lifecycleScope.launch {
-            source.updates.collect {
-                Log.i("test_", "test_ collect updates: $it")
-                textView2.text = "Update: $it"
+            source1.get("1", FromCache.NEVER).collect {
+                textView1.text = "Ready, invocation count: $it"
             }
         }
     }
 
+    private fun refresh2() {
+        lifecycleScope.launch {
+            source1.get("2", FromCache.NEVER)
+                .collect {
+                    textView2.text = "Ready, invocation count: $it"
+                }
+        }
+    }
 }
